c9b09a6f746ad82fe2556c6ced3a3a95674bd20d
acbrent25 <adamchampagne@gmail.com>
2018-02-10 05:52:20 -0500

    setup intial files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Recipe Saver
+ * Description:       A simple worpdress plugin that allows you create and save recipes
+ * Version:           1.0
+ * Author:            Adam Champagne
+ * Author URI:        http://adamchampagne.com/
+ * Text Domain:       recipe
+ */
+
